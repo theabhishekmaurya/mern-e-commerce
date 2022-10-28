d7a4ee0f85ad2693aaa06dfbf0a7170404a5d89a
anchor-abhi <theabhishek1802@gmail.com>
2022-10-29 01:23:04 +0530

    deployment test

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,7 +2,5 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 module.exports = async () => {
-  return mongoose.connect(
-    "mongodb+srv://abhishek:abhishek123@cluster0.a3v1rlc.mongodb.net/?retryWrites=true&w=majority"
-  );
+  return mongoose.connect(process.env.MONGODB_URI);
 };
