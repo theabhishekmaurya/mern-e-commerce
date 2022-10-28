b3526435467ce0433f24ca9fee51cbf01aacdeba
anchor-abhi <theabhishek1802@gmail.com>
2022-10-29 00:30:07 +0530

    db update

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,5 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 module.exports = async () => {
-  return mongoose.connect(process.env.MONGODB_URI);
+  return mongoose.connect(
+    "mongodb+srv://abhishek:abhishek123@cluster0.a3v1rlc.mongodb.net/?retryWrites=true&w=majority"
+  );
 };
