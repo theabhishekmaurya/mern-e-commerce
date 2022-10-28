f59ed5886112644ad5a1e919a2638679efc6ea16
anchor-abhi <theabhishek1802@gmail.com>
2022-10-28 22:20:10 +0530

    changes in fil structure

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors");
-const userController = require("../controllers/user.controller");
-const adminController = require("../controllers/admin.controller");
-const cartController = require("../controllers/cart.controller");
-const addressController = require("../controllers/address.controller");
-const orderController = require("../controllers/order.controller");
-const connect = require("../configs/db");
+const userController = require("./controllers/user.controller");
+const adminController = require("./controllers/admin.controller");
+const cartController = require("./controllers/cart.controller");
+const addressController = require("./controllers/address.controller");
+const orderController = require("./controllers/order.controller");
+const connect = require("./configs/db");
 const path = require("path");
 
 const PORT = process.env.port || 5000;
